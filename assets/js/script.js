7bb2d6ea75c1af79b6ccf044d19cc138eb8afe60
AndrewH1188 <andrewh1188@gmail.com>
2025-05-26 14:36:41 +0000

    Added slick file, updates to HTML and SCSS styling updates.

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.l-slick').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      dots: true,
+      arrows: true
+  });
+});
